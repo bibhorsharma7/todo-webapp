f93f5b6f350997a8891c453dacda89d964c2dc4f
Bibhor Sharma <bibhorsharma7@gmail.com>
2024-09-13 09:17:57 +1000

    stash: work in progress

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -4,11 +4,10 @@ interface ButtonProps {
   onClick: () => void;
 }
 
-
 export default function Button({ name, onClick, className }: ButtonProps) {
   return (
     <div
-      className={`${className ?? ''} text-center rounded-md cursor-pointer border-black bg-green-800 text-white hover:bg-green-700`}
+      className={`${className ?? ""} text-center rounded-md cursor-pointer border-black bg-green-800 text-white hover:bg-green-700`}
       onClick={onClick}
     >
       {name}
--- app/components/task.tsx
+++ app/components/task.tsx
@@ -1,3 +1,9 @@
+import {
+  EllipsisHorizontalIcon,
+  XCircleIcon,
+  CheckCircleIcon,
+} from "@heroicons/react/24/solid";
+
 interface TaskProps {
   id: number;
   title: string;
@@ -5,26 +11,33 @@ interface TaskProps {
   status: "pending" | "completed";
 }
 
-export default function Task({ title, description }: TaskProps) {
+export default function Task({ title, description, status }: TaskProps) {
   return (
     <div className="flex flex-row rounded-md border-black border p-2 m-2 ">
       <div className="flex flex-col flex-1">
         <p className="text-bold text-md">{title}</p>
         <p className="text-sm">{description}</p>
       </div>
-      <div className="w-auto flex flex-row" onClick={() => console.log("Delete task clicked")}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="#eb3434"
-          className="w-6 h-6 hover:cursor-pointer"
+      <div className="flex flex-row items-center space-x-2">
+        <div className="flex flex-col text-xs">
+          {status == "pending" ? (
+            <>
+              <EllipsisHorizontalIcon className="size-6 text-blue-500" />
+              <label>Pending</label>
+            </>
+          ) : (
+            <>
+              <CheckCircleIcon className="size-6 text-green-600" />
+              <label>Completed</label>
+            </>
+          )}
+        </div>
+        <div
+          className="w-auto"
+          onClick={() => console.log("Delete task clicked")}
         >
-          <path
-            fillRule="evenodd"
-            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z"
-            clipRule="evenodd"
-          />
-        </svg>
+          <XCircleIcon className="size-6 text-red-500" />
+        </div>
       </div>
     </div>
   );
