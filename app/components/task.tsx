8d6636c70ab77a634abbf74d80ed0f55c3ac448b
bibhor-evergreen <bibhor@evergreenconsultants.com.au>
2024-09-13 11:44:37 +1000

    fix: added reusable component taskControl

--- app/components/task.tsx
+++ app/components/task.tsx
@@ -2,7 +2,24 @@ import {
   EllipsisHorizontalIcon,
   XCircleIcon,
   CheckCircleIcon,
+  PencilSquareIcon,
 } from "@heroicons/react/24/solid";
+import { ReactNode, useState } from "react";
+
+function TaskControl({
+  children,
+  label,
+}: {
+  children: ReactNode;
+  label: string;
+}) {
+  return (
+    <div className="flex flex-col items-center text-slate-400 ">
+      {children}
+      <p className="capitalize">{label}</p>
+    </div>
+  );
+}
 
 export interface TaskProps {
   id: string;
@@ -13,35 +30,41 @@ export interface TaskProps {
   onToggle: () => void;
 }
 
-export default function Task({ title, description, status, onDelete, onToggle }: TaskProps) {
+export default function Task({
+  title,
+  description,
+  status,
+  onDelete,
+  onToggle,
+}: TaskProps) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="flex flex-row rounded-md border-black border p-2 m-2 ">
       <div className="flex flex-col flex-1">
         <p className="text-bold text-md">{title}</p>
         <p className="text-sm">{description}</p>
       </div>
-      <div className="flex flex-row align-text-bottom space-x-2">
-        <div
-          onClick={onToggle}
-          className="flex flex-col text-xs text-center items-center">
+      <div className="flex flex-row align-text-bottom space-x-2 text-xs">
+        <TaskControl label="Edit">
+          <PencilSquareIcon className="size-7 text-yellow-200" />
+        </TaskControl>
+        <TaskControl label={status}>
           {status == "pending" ? (
-            <>
-              <EllipsisHorizontalIcon className="size-6 text-blue-500" />
-              <label>Pending</label>
-            </>
+            <EllipsisHorizontalIcon
+              onClick={onToggle}
+              className="size-7 text-blue-500"
+            />
           ) : (
-            <>
-              <CheckCircleIcon className="size-6 text-green-600" />
-              <label>Completed</label>
-            </>
+            <CheckCircleIcon
+              onClick={onToggle}
+              className="size-7 text-green-600"
+            />
           )}
-        </div>
-        <div
-          className="w-auto"
-          onClick={onDelete}
-        >
-          <XCircleIcon className="size-6 text-red-500" />
-        </div>
+        </TaskControl>
+        <TaskControl label="Delete">
+          <XCircleIcon onClick={onDelete} className="size-7 text-red-500" />
+        </TaskControl>
       </div>
     </div>
   );
