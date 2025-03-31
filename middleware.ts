e044a674953ecc4980af1ec473c150ac559ec387
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-03-31 12:59:07 +1100

    add: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,11 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  const isLoggedIn = req.auth;
+  console.log(isLoggedIn);
+  console.log(req.nextUrl.pathname);
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
+};
