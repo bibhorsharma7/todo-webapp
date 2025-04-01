4f61199c99b74439d5aa6e6ea78a49dcfc3a3e3c
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-04-01 13:03:40 +1100

    fix: prisma with edge error

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import authConfig from "@/auth.config";
 import { prisma } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
