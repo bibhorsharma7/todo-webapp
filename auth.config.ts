30cda00b6ac1c373324500bd55822e3f16da1699
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-04-03 13:16:22 +1100

    add: github sigin

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,9 @@
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 import { type NextAuthConfig } from "next-auth";
 
 const config: NextAuthConfig = {
-  providers: [Google],
+  providers: [Google, Github],
 };
 
 export default config;
