d858333b055944f4002a352209fe36b50f59dc84
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-04-03 13:13:05 +1100

    fix: add authentication to get and post api routes

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,21 +1,51 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/auth";
 
 export async function GET() {
-  // get all todos form database
-  const todos = await prisma.task.findMany();
+  // Get the authenticated user's session
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ status: 401, message: "Unauthorized" });
+  }
+
+  const userId = session.user.id;
+  console.log("userId:", userId);
+
+  // Get tasks related to the authenticated user
+  const todos = await prisma.task.findMany({
+    where: { userId: userId },
+  });
+
   return NextResponse.json({ status: 200, tasks: todos });
 }
 
 export async function POST(request: NextRequest) {
-  // add new todo
   const body = await request.json();
+
+  // Get the authenticated user's session
+  const session = await auth();
+  if (!session || !session.user || !session.user.id) {
+    return NextResponse.json({ status: 401, message: "Unauthorized" });
+  }
+
+  const userId = session.user.id;
+  console.log("userId:", userId);
+
   const data = {
     title: body.title,
     description: body.description,
+    userId: userId,
   };
 
-  await prisma.task.create({ data });
-
-  return NextResponse.json({ status: 200, message: "Successful" });
+  try {
+    await prisma.task.create({ data });
+    return NextResponse.json({
+      status: 200,
+      message: "Task created successfully",
+    });
+  } catch (error) {
+    console.error("Error creating task:", error);
+    return NextResponse.json({ status: 500, message: "Internal Server Error" });
+  }
 }
