b45c4f54b6b729fc54ab181355a5b1cd794cbd55
Bibhor Sharma <bibhorsharma7@gmail.com>
2025-04-03 13:15:11 +1100

    add: protect all routes

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,24 @@ import authConfig from "@/auth.config";
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  const isLoggedIn = req.auth;
-  console.log(isLoggedIn);
-  console.log(req.nextUrl.pathname);
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  console.log("Logged In:", isLoggedIn);
+  console.log("url:", nextUrl.pathname);
+
+  const isApiRoute = nextUrl.pathname.startsWith("/api");
+  if (isApiRoute) {
+    return;
+  }
+
+  // redirect not logged in user to login page
+  if (!isLoggedIn && nextUrl.pathname !== "/signin") {
+    return Response.redirect(new URL("/signin", nextUrl));
+  }
+
+  if (nextUrl.pathname === "/signin" && isLoggedIn) {
+    return Response.redirect(new URL("/", nextUrl));
+  }
 });
 
 export const config = {
